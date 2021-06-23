dae2beaa82966ab92fa61ad4d4b93269076668f8
IbethOrtega <66796601+IbethOrtega@users.noreply.github.com>
2021-06-23 15:43:53 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL', 'http://35.222.252.78/articles/');
+define('URL', 'http://34.69.44.48/articles/login');
 define('ROOT', $_SERVER['DOCUMENT_ROOT'].'/articles/');
 
 define('HOST', 'localhost');
